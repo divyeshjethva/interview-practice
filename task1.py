fab03d582d3ceb46c64078e399f0bbf98b0cb81a
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-09 14:21:28 +0530

    count vowels

--- task1.py
+++ task1.py
@@ -1,9 +1,21 @@
 
-s = input("Enter name :")
+# s = input("Enter name :")
 
-a = len(s) // 2
+# a = len(s) // 2
 
-if len(s)%2 == 0:
-    print("enter odd string")
-else:
-    print( s[a-1] + s[a] + s[a+1])
+# if len(s)%2 == 0:
+#     print("enter odd string")
+# else:
+#     print( s[a-1] + s[a] + s[a+1])
+
+# ================================
+
+vowels = "aeiou"
+
+user = input("enter name :")
+total = 0
+
+for i in user:
+    if i in vowels:
+        total += 1
+print("total vowes is :", total)
