57184ddd52f15bd11d1bb3c9868ed68b798a125d
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-09 20:19:15 +0530

    find largest number

--- task1.py
+++ task1.py
@@ -192,3 +192,18 @@
 
 # print(even)
 # print(odd)
+
+# ===========================
+
+# n1 = int(input("Enter number1 :"))
+# n2 = int(input("Enter number2 :"))
+# n3 = int(input("Enter number3 :"))
+
+# if n1>n2 and n1>n3:
+#     print(n1,"is greater")
+# elif n2>n3:
+#     print(n2,"is greater")
+# else:
+#     print(n3,"is greater")
+    
+    
