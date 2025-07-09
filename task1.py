d70f2111fa6438a432cca8c37d0f1f7e509bd52c
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-09 21:11:51 +0530

    random module

--- task1.py
+++ task1.py
@@ -268,7 +268,16 @@
 
 # =====================================
 
-name = input("Enter name :")
-f = open("demo.txt","a")
-f.write(f"hello {name}")
-f.close()
+# name = input("Enter name :")
+# f = open("demo.txt","a")
+# f.write(f"hello {name}")
+# f.close()
+# =====================================
+
+# import random
+
+# a = random.randint(1,10)
+# print(a)
+
+# b = [5,6,8,7,9,3,4,5]
+# print(random.choice(b))
