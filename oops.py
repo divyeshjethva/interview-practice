5c2165f35f6fccc5b6eb39ad8aa3ef22b0365c4f
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-10 23:55:49 +0530

    encapulation

--- oops.py
+++ oops.py
@@ -116,3 +116,20 @@
         
 # obj = d()
 # obj.fun1()
+
+# ===================================
+
+# encapulation
+
+# class a:
+#     def fun1(self):
+#         a = 10
+#         self.a = a
+        
+# class b(a):
+#     def fun2(self):
+#         print(self.a)
+        
+# obj = b()
+# obj.fun1()
+# obj.fun2()
