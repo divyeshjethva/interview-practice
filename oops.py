6ac6bb5b83f0fd44f404e2f1370732c145d7e270
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-10 23:40:43 +0530

    multilevel inheritance

--- oops.py
+++ oops.py
@@ -24,4 +24,18 @@
 # obj.fun1()
 # obj.fun2()
 # --------------------------
-# multiple
+
+# multilevel
+# class a:
+#     def fun1(self):
+#         print("method 1")
+# class b(a):
+#     def fun2(self):
+#         print("method 2")
+# class c(b):
+#     def fun3(self):
+#         print("method 3")
+# obj = c()
+# obj.fun1()
+# obj.fun2()
+# obj.fun3()
