976b98a38e2d685fbf0ccc0289351567818f2fe6
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-10 23:47:04 +0530

    hybrid inheritance

--- oops.py
+++ oops.py
@@ -56,18 +56,40 @@
 # obj.fun3()
 # -------------------------
 # hirachical
-class a:
-    def fun1(self):
-        print("method 1")
-class b(a):
-    def fun2(self):
-        print("method 2")
-class c(a):
-    def fun3(self):
-        print("method 3")
-obj = b()
-obj.fun1()
-obj.fun2()
-obj = c()
-obj.fun1()
-obj.fun3()
+# class a:
+#     def fun1(self):
+#         print("method 1")
+# class b(a):
+#     def fun2(self):
+#         print("method 2")
+# class c(a):
+#     def fun3(self):
+#         print("method 3")
+# obj = b()
+# obj.fun1()
+# obj.fun2()
+# obj = c()
+# obj.fun1()
+# obj.fun3()
+# -------------------------
+
+# hybrid
+
+# class a:
+#     def fun1(self):
+#         print("method 1")
+# class b(a):
+#     def fun2(self):
+#         print("method 2")
+# class c:
+#     def fun3(self):
+#         print("method 3")
+# class d(b,c):
+#     def fun4(self):
+#         print("method 4")
+        
+# obj = d()
+# obj.fun1()
+# obj.fun2()
+# obj.fun3()
+# obj.fun4()
