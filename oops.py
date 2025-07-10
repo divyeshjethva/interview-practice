b9581c2f1c265bf03de6e1cc3a7705ac79d90982
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-10 23:37:18 +0530

    single inheritance

--- oops.py
+++ oops.py
@@ -6,3 +6,22 @@
 
 # obj = a()
 # obj.fun1()
+
+# =======================
+
+# inheritance 
+# ------------------------
+
+# single
+# class a:
+#     def fun1(self):
+#         print("method 1")
+# class b(a):
+#     def fun2(self):
+#         print("method 2")
+        
+# obj = b()
+# obj.fun1()
+# obj.fun2()
+# --------------------------
+# multiple
