40a89685990d1e7fa3e82b535e10ac1a50386107
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-11 00:08:45 +0530

    start bank manage structer

--- bank.py
+++ bank.py
@@ -1,1 +1,32 @@
 # bank management system - python 
+
+d = {}
+
+class bank:
+    def create(self):
+        pass
+    def withdrow(self):
+        pass
+    def deposite(self):
+        pass
+    
+obj = bank()
+
+while True:
+    menu = """
+        1) create account
+        2) withdrow account
+        3) deposite account
+        4) exit
+    """
+    print(menu)
+    
+    choice = int(input("Enter your choice :"))
+    if choice == 1:
+        obj.create()
+    elif choice == 2:
+        obj.withdrow()
+    elif choice == 3:
+        obj.deposite()
+    else:
+        print("Thank you")
