471d4d06f1093106f43253a433bcd2f17f6588ca
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-10 23:42:06 +0530

    multiple inheritance

--- oops.py
+++ oops.py
@@ -39,3 +39,18 @@
 # obj.fun1()
 # obj.fun2()
 # obj.fun3()
+# ---------------------------
+# multiple
+class a:
+    def fun1(self):
+        print("method 1")
+class b:
+    def fun2(self):
+        print("method 2")
+class c(a,b):
+    def fun3(self):
+        print("method 3")
+obj = c()
+obj.fun1()
+obj.fun2()
+obj.fun3()
