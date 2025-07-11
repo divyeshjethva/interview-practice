c9c1bd3b6f7b2981c24383ee4801a5de01823a91
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-11 09:06:10 +0530

    bank management sysytem

--- bank.py
+++ bank.py
@@ -3,30 +3,68 @@
 d = {}
 
 class bank:
-    def create(self):
-        pass
+    def create(self,index):
+        name = input("enter name :")
+        balance = int(input("enter balance :"))
+        
+        d[index] = {"Name":name,"balance":balance}
+        print("ACCOUNT CREATE SUCCESSFULLY")
+        print("your account number is :",index)
+        
     def withdrow(self):
-        pass
+        acc_no = int(input("enter account no. :"))
+        for i,j in d.items():
+            if i == acc_no:
+                for k,v in j.items():
+                    if k == "balance":
+                        amount = int(input("Enter amount to withdrow"))
+                        a = v - amount
+                        d[acc_no] = {"balance":a}
+                        print("your current balance :",a)
+        
     def deposite(self):
-        pass
+        acc_no = int(input("enter account no. :"))
+        for i,j in d.items():
+            if i == acc_no:
+                for k,v in j.items():
+                    if k == "balance":
+                        amount = int(input("Enter amount to deposite"))
+                        a = v + amount
+                        d[acc_no] = {"balance":a}
+                        print("your current balance :",a)
+                        
+    def view(self):
+        for i,j in d.items():
+            print("account nomber :", i)
+            for k,v in j.items():
+                print(k,":",v)
+            print("-----------------")
     
 obj = bank()
+index = 1
 
 while True:
     menu = """
         1) create account
         2) withdrow account
         3) deposite account
-        4) exit
+        4) view account
+        5) exit
     """
     print(menu)
     
     choice = int(input("Enter your choice :"))
     if choice == 1:
-        obj.create()
+        try:
+            obj.create(index)
+            index+=1
+        except:
+            print("some error")
     elif choice == 2:
         obj.withdrow()
     elif choice == 3:
         obj.deposite()
+    elif choice == 4:
+        obj.view()
     else:
         print("Thank you")
