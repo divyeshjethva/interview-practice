f22256fa76595b47d73dac8aba7edbb405b2f580
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-23 00:09:09 +0530

    star pettarn '*'

--- star pettern/task.py
+++ star pettern/task.py
@@ -20,4 +20,36 @@
 
 # --------------------------------
 
-for 
+# for i in range(1,6):
+#     for j in range(6-i):
+#         print(" ",end='')
+#     for n in range(i):
+#         print("*",end=" ")
+#     print()
+    
+# --------------------------------
+
+# for i in range(1,6):
+#     for j in range(6-i):
+#         print("*", end="")
+#     print()
+    
+# --------------------------------
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print(" ",end='')
+#     for n in range(6-i):
+#         print("*",end="")
+#     print()
+
+# --------------------------------
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print(" ",end='')
+#     for n in range(6-i):
+#         print("*",end=" ")
+#     print()
+        
+# --------------------------------
