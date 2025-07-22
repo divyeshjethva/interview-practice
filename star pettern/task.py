7b3d7d2a31cb4907ee43a4b539d3a2a77388e275
divyeshjethva <divyeshjethva2004@gmail.com>
2025-07-23 00:18:46 +0530

    star pettarn with numeric

--- star pettern/task.py
+++ star pettern/task.py
@@ -53,3 +53,10 @@
 #     print()
         
 # --------------------------------
+
+for i in range(1,6):
+    for n in range(6-i):
+        print(" ",end='')
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print()
