a050f1d113d69ea768f2d1ae83fc962f5efbbd7f
divyeshjethva <162811798+divyeshjethva@users.noreply.github.com>
2025-09-04 23:06:41 +0530

    Create find largest number.py

---
+++ Imp/find largest number.py
@@ -0,0 +1,13 @@
+#find largest number in list 
+# bubble sort
+
+l = [21,400,63,22,65,32,90]
+n = len(l)
+
+for i in range(n):
+    for j in range(0,n-i-1):
+        if l[j]<l[j+1]:
+            l[j], l[j+1]=l[j+1],l[j]
+    print(l)
+            
+print(l[0])
