39c37ec0453bbff24acdd88ca4d7d213fb65672c
divyeshjethva <divyeshjethva2004@gmail.com>
2025-09-05 14:31:52 +0530

    without loop factorial number

---
+++ Imp/factorial.py
@@ -0,0 +1,23 @@
+
+# normal factorial
+# ---------------------------
+# n = 5
+# fact = 1
+
+# for i in range(1,6):
+#     fact = fact * i
+    
+# print(fact)
+
+
+# without loop factorial
+# -----------------------------
+
+def fun(n,f=1):
+    if n == 0:
+        print(f)
+        return
+    f*=n
+    fun(n-1,f)
+fun(5)
+
