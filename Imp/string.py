503e8ddcaad319a922fdd3bc74025d56c72e578f
divyeshjethva <divyeshjethva2004@gmail.com>
2025-09-07 15:27:48 +0530

    string

--- Imp/string.py
+++ Imp/string.py
@@ -1,8 +1,16 @@
-s = "abcabcbb"
-l = []
-for i in s:
-    if i in l:
-        pass
-    else:
-        l.append(i)
-print(l)
+# s = "abcabcbb"
+# l = []
+# for i in s:
+#     if i in l:
+#         pass
+#     else:
+#         l.append(i)
+# print(l)
+
+# ----------------------------------------
+
+# s = "python programing"
+
+# a = s.split()
+# print(a[1],a[0])
+# --------------------------------
