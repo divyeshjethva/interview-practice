12402b66f8bb2b4e8fd17f3b489c794f75859901
divyeshjethva <divyeshjethva2004@gmail.com>
2025-09-07 16:15:37 +0530

    find largest value on dectonary

---
+++ Imp/dictonary.py
@@ -0,0 +1,13 @@
+# -------------------------------
+# find largest value on dictonary
+
+# d = {'a':10,'b':40,'c':20}
+# a = max(d.values())
+# d1 = {}
+# for k,v in d.items():
+#     if v==a:
+#         d1[k] = v
+        
+# print(d1)
+# ------------------------------
+
