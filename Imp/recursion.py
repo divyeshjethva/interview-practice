f4914d81347bd9b9df3039b69bcfb0841170fc69
divyeshjethva <divyeshjethva2004@gmail.com>
2025-09-07 14:15:34 +0530

    recursion.py

---
+++ Imp/recursion.py
@@ -0,0 +1,47 @@
+
+# --------------------------------------
+# prime number
+# prime = 0
+# n = 7
+# for i in range(1,n+1):
+#     if n%i == 0:
+#         prime+=1
+
+# if prime == 2:
+#     print("Prime")
+# else:
+#     print("Not")
+# ---------------------------
+
+# prime number without loop
+
+# def f(n,p,i):
+#     if i == n:
+#         if p == 1:
+#             print("prime")
+#         else:
+#             print("Not")
+#         return
+#     if n%i==0:
+#         p+=1
+#     f(n,p,i+1)
+
+# f(11,0,1)
+
+# ---------------------------
+
+# list to find prime number
+
+# l = [5,8,7,11,4,6]
+# s = []
+# for i in l:
+#     prime = 0
+#     for j in range(1,i+1):
+#         if i%j == 0:
+#             prime +=1
+#     if prime == 2:
+#         s.append(i)
+# print(s)
+
+
+
