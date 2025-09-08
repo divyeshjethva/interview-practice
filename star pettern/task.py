c88f6d4aedb5beddfe8c1c891c8423870fa75359
divyeshjethva <divyeshjethva2004@gmail.com>
2025-09-08 12:25:19 +0530

    star pettrn with alfabets

--- star pettern/task.py
+++ star pettern/task.py
@@ -54,9 +54,39 @@
         
 # --------------------------------
 
+# for i in range(1,6):
+#     for n in range(6-i):
+#         print(" ",end='')
+#     for j in range(1,i+1):
+#         print(j,end=' ')
+#     print()
+
+# --------------------------
+# n = 1
+# for row in range(1, 6):
+#     for col in range(row):
+#         if n == 1:
+#             print("d",end=' ')
+#         elif n == 5:
+#             print("i",end=' ')
+#         elif n == 11:
+#             print("v",end=' ')
+#         elif n == 15:
+#             print("y",end=' ')
+#         else:
+#             print("*", end=' ')
+#         n += 1
+#     print()
+
+# --------------------------
+n = 15
 for i in range(1,6):
-    for n in range(6-i):
-        print(" ",end='')
-    for j in range(1,i+1):
-        print(j,end=' ')
+    for j in range(6-i):
+        if n == 12:
+            print("D",end=' ')
+        elif n == 5:
+            print("I",end=' ')
+        else:
+            print("*",end=' ')
+        n -=1
     print()
