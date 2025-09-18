6b3b35009e7a08a1676612b24c58be222efb6de2
divyeshjethva <divyeshjethva2004@gmail.com>
2025-09-18 12:28:46 +0530

    chat gpt task

---
+++ chat gpt/task1.py
@@ -0,0 +1,13 @@
+
+# bubble sort
+
+# l = [2,8,7,3,5,9,4]
+
+# n = len(l)
+# for i in range(n):
+#     for j in range(0,n-i-1):
+#         if l[j]>l[j+1]:
+#             l[j],l[j+1] = l[j+1],l[j]
+# print(l)
+
+# =====================================
