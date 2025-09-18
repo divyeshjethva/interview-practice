66d1c2cb232bdf1761e1e3fff28cdcc24ca78575
divyeshjethva <divyeshjethva2004@gmail.com>
2025-09-18 13:25:14 +0530

    interview task

---
+++ chat gpt/task2.py
@@ -0,0 +1,84 @@
+
+# s = "divyesh"
+
+# n = len(s)//2
+
+# if n%2 == 0:
+#     print("enter even string")
+# else:
+#     print(s[0], s[n], s[-1])
+
+# print(n)
+# ------------------------------
+# s = "aaaaa"
+# v = "aeiou"
+# c = 0
+
+# for i in s:
+#     if i in v:
+#         c+=1
+# print(c)
+# ---------------------------------
+
+# n = "nayan"
+
+# r = n[::-1]
+
+# if n == r:
+#     print("pelidrom")
+# else:
+#     print("Not")
+# ----------------
+
+# n = "wow"
+# r = ""
+# for i in range(len(n)-1,-1,-1):
+#     r+=n[i]
+    
+# if n == r:
+#     print("p")
+# else:
+#     print("n")
+
+# ----------------
+
+# s = "bob"
+# g = ""
+# def fun(n):
+#     if n == -1:
+#         return
+#     global g
+#     g+=s[n]
+#     fun(n-1)    
+# fun(len(s)-1)
+
+# if g == s:
+#     print("pelidrom")
+# else:
+#     print("Not")
+
+# -------------------------------------------
+
+# n = input("Enter numer :")
+# s = 0
+# for i in n:
+#     s+=int(i)**3
+    
+# if s== int(n):
+#     print("A")
+# else:
+#     print("N")
+
+# -----------------------------------------------
+
+# n = 10
+# a = 0
+# b = 1
+# l = []
+# for i in range(1,11):
+#     l.append(a)
+#     a,b = b,a+b
+    
+# print(l)
+# -------------
+
